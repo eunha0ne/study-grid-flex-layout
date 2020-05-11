872942cd290e8cfee3c77e32fee22b7410d923b2
eunha0ne <eunha0ne@gmail.com>
2020-05-12 01:22:14 +0900

    [Feat] 컨테이너 컴포넌트 추가

--- src/components/LayoutFlex.vue
+++ src/components/LayoutFlex.vue
@@ -1,50 +1,28 @@
 <template>
-  <section>
-    <h2>DISPLAY: FLEX</h2>
-    <div class="container container--flex">
-      <div v-for="i in 5" :key="i" :class="`item item--${i}`">
-        <span class="item__tag">ITEM--{{ i }}</span>
-      </div>
-    </div>
-  </section>
+  <Container display-type="flex" />
 </template>
 
 <script>
+import Container from './Container'
+
 export default {
-  name: 'LayoutGrid'
+  name: 'LayoutFlex',
+  components: {
+    Container
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-@import '@/assets/styles/container';
-@import '@/assets/styles/item';
-
-.container {
-  &--flex {
-    display: flex;
-    /* flex-direction: row; */
-    /* flex-wrap: nowrap; */
-    /* Short-hand: */
-    flex-flow: row wrap;
-    justify-content: flex-start;
-  }
+::v-deep .container--flex {
+  display: flex;
+  flex-flow: row wrap; // Short-hand: flex-direction | flex-wrap
+  justify-content: flex-start;
 }
 
-.item {
-  margin: 15px;
-  flex: 0 0 calc(33.33% - 30px);
+::v-deep .item {
+  margin: 5px;
+  flex: 1 1 calc(33.33% - 10px);
   height: 150px;
-
-  &:first-of-type {
-    margin-left: 0;
-  }
-
-  &:last-of-type {
-    margin-right: 0;
-  }
-
-  &:nth-of-type(3n + 1) {
-    margin-left: 0;
-  }
 }
 </style>
--- src/components/LayoutGrid.vue
+++ src/components/LayoutGrid.vue
@@ -1,33 +1,24 @@
 <template>
-  <section>
-    <h2>DISPLAY: GRID</h2>
-    <div class="container container--grid">
-      <div v-for="i in 5" :key="i" :class="`item item--${i}`">
-        <span class="item__tag">ITEM--{{ i }}</span>
-      </div>
-    </div>
-  </section>
+  <Container display-type="grid" />
 </template>
 
 <script>
+import Container from './Container'
+
 export default {
-  name: 'LayoutGrid'
+  name: 'LayoutGrid',
+  components: {
+    Container
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-@import '@/assets/styles/container';
-@import '@/assets/styles/item';
-
-.container {
-  &--grid {
-    display: grid;
-    /* grid-template-columns: 250px 250px 250px 250px 250px 250px; */
-    /* Short-hand: */
-    /* grid-template-columns: repeat(5, 250px); */
-    grid-template-columns: 1fr 2fr 3fr;
-    grid-template-rows: 100px 200px;
-    grid-gap: 30px;
-  }
+::v-deep .container--grid {
+  display: grid;
+  // grid-template-columns: repeat(5, 250px); // Short-hand: 250px 250px 250px 250px 250px 250px;
+  grid-template-columns: 1fr 2fr 3fr;
+  grid-template-rows: 100px 200px;
+  grid-gap: 30px;
 }
 </style>
