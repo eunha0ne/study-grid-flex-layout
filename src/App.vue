31acdc8d4d518942f81997ead63b74bb43860763
eunha0ne <eunha0ne@gmail.com>
2020-05-11 20:22:07 +0900

    [Chore] 기본 컴포넌트 생성과 컨벤션 정리

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LayoutGrid />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LayoutGrid from './components/LayoutGrid.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LayoutGrid
   }
 }
 </script>
---
+++ src/components/LayoutGrid.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container container--gird">
+    <div v-for="i in 5" :key="i">item-{{ i }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LayoutGrid'
+}
+</script>
+
+<style scoped></style>
